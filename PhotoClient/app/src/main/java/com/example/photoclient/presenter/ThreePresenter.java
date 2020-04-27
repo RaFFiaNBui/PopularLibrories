86d4472dacf39fcfb2881d34f79292e4aebd22ca
RaFFiaNBui <Raffian_bui@mail.ru>
2020-04-27 14:13:54 +0300

    ДЗ №4 было mvp + moxy + RecyclerView + Rxjava + Butterknife реализация Retrofit + Gson + Picasso

--- PhotoClient/app/src/main/java/com/example/photoclient/presenter/ThreePresenter.java
+++ PhotoClient/app/src/main/java/com/example/photoclient/presenter/ThreePresenter.java
@@ -1,36 +1,63 @@
 package com.example.photoclient.presenter;
 
-import com.example.photoclient.model.Data;
+import android.util.Log;
+
+import com.example.photoclient.model.gson.Hit;
+import com.example.photoclient.model.gson.Photo;
+import com.example.photoclient.model.retrofit.ApiRequest;
 import com.example.photoclient.view.IViewHolder;
 import com.example.photoclient.view.MoxyView;
 
 import java.util.List;
 
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
 import moxy.InjectViewState;
 import moxy.MvpPresenter;
 
 @InjectViewState
-public class ThreePresenter extends MvpPresenter<MoxyView> implements IRecyclerThreePresenter {
+public class ThreePresenter extends MvpPresenter<MoxyView> implements RecyclerPresenter {
 
-    private List<String> list;
+    private static final String TAG = "tag";
+
+    private List<Hit> hitList;
+    private ApiRequest api;
 
     public ThreePresenter() {
-        Data data = new Data();
-        this.list = data.getList();
+        this.api = new ApiRequest();
+    }
+
+    @Override
+    protected void onFirstViewAttach() {
+        getAllPhoto();
+    }
+
+    private void getAllPhoto() {
+        Observable<Photo> single = api.requestServer();
+
+        Disposable disposable = single.observeOn(AndroidSchedulers.mainThread()).subscribe(photo -> {
+                    hitList = photo.hits;
+                    getViewState().updateRecyclerView();
+                }, throwable -> Log.d(TAG, "getAllPhoto: Error" + throwable)
+        );
     }
 
     @Override
     public void bindView(IViewHolder holder) {
-        holder.setImage();
+        holder.setImage(hitList.get(holder.getPos()).webformatURL);
     }
 
     @Override
     public int getItemCount() {
-        return list.size();
+        if (hitList != null) {
+            return hitList.size();
+        }
+        return 0;
     }
 
     @Override
-    public void counter(int position) {
-        getViewState().showPicture(position);
+    public void getUrl(String url) {
+        getViewState().showPicture(url);
     }
 }
