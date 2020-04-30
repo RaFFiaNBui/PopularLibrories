b58a16608e002b6529c89b0def0cafe1334d63e3
RaFFiaNBui <Raffian_bui@mail.ru>
2020-04-30 23:03:26 +0300

    ДЗ №5 было mvp + moxy + RecyclerView + Rxjava + Butterknife + Retrofit + Gson + Picasso реализация Room

--- PhotoClient/app/src/main/java/com/example/photoclient/model/gson/Hit.java
+++ PhotoClient/app/src/main/java/com/example/photoclient/model/gson/Hit.java
@@ -1,9 +1,17 @@
 package com.example.photoclient.model.gson;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+@Entity(tableName = "urls")
 public class Hit {
+
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+
     @Expose
     @SerializedName("webformatURL")
     public String webformatURL;
