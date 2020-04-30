8ae5df858eee3e9ffd8be785f512908fa9fd5adf
RaFFiaNBui <Raffian_bui@mail.ru>
2020-04-30 23:04:16 +0300

    ДЗ №5 было mvp + moxy + RecyclerView + Rxjava + Butterknife + Retrofit + Gson + Picasso реализация Room

---
+++ PhotoClient/app/src/main/java/com/example/photoclient/model/room/UrlDao.java
@@ -0,0 +1,22 @@
+package com.example.photoclient.model.room;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import com.example.photoclient.model.gson.Hit;
+
+import java.util.List;
+
+import io.reactivex.Single;
+
+@Dao
+public interface UrlDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    Single<List<Long>> addUrlList(List<Hit> urlList);
+
+    @Query("SELECT * FROM urls")
+    Single<List<Hit>> getAll();
+}
