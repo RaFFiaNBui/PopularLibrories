0866dda86e0f25557cfa1de1eede3fa20d9ba2cc
RaFFiaNBui <Raffian_bui@mail.ru>
2020-05-01 13:36:41 +0300

    ДЗ №5 было mvp + moxy + RecyclerView + Rxjava + Butterknife + Retrofit + Gson + Picasso реализация Room: +listener на ошибку загрузки изображения

--- PhotoClient/app/src/main/java/com/example/photoclient/model/PicassoLoader.java
+++ PhotoClient/app/src/main/java/com/example/photoclient/model/PicassoLoader.java
@@ -1,15 +1,27 @@
 package com.example.photoclient.model;
 
+import android.util.Log;
 import android.widget.ImageView;
 
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
 public class PicassoLoader {
 
-    public void loadImage(String url, ImageView imageView) {
+    public void loadImage(String url, ImageView imageView, ErrorLoadingListener errorLoadingListener) {
         Picasso
                 .get()
                 .load(url)
-                .into(imageView);
+                .into(imageView, new Callback() {
+                    @Override
+                    public void onSuccess() {
+                    }
+
+                    @Override
+                    public void onError(Exception e) {
+                        Log.e("MyTag", "onError: ", e);
+                        errorLoadingListener.onErrorLoading();
+                    }
+                });
     }
 }
